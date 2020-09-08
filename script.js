1e779730dbe77f273ebb74884b7dc7db69033cca
Champion113 <sbalbright19@gmail.com>
2020-09-08 15:28:36 -0400

    timeblock colors

--- script.js
+++ script.js
@@ -3,24 +3,42 @@ $(document).ready(function(){
 
 $(".saveBtn").on("click", function(){
 // things that happen on the button
-var activity = $(this).siblings(".description").val();
-
-console.log(activity)
+    var activity = $(this).siblings(".description").val();
+    console.log(activity)
 })
 
 //  funtion to be able to detect time using moments;
-$("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
+    $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
+
+// moment time function
+    var currentTime = moment().format('H');
+    var currentHour = moment().hours();
 
 // funtion to determine if time past present or future within hour timeframe
-var currentTime = moment().format('H');
-// set colors based on timeframe p-grey pr-green f-red
-$('.colorcode').each(function name(params) {
-    
-})
+ 
+    $(".row-timeblocks").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[ 1 ]);
+
+// loop over timeblock
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    }
+    else if (blockHour === currentHour) {
+        $(this).addClass("present");
+    }
+    else {
+        $(this).addClass("future");
+        }
+    });
+    });
 
 
 
 //save to local storage
-//localStorage.setItem('col-md-10 description', JSON.stringify(list));
+    var loadTasks = function() {
+    var list = JSON.parse(localStorage.getItem("col-md-10 description")) || [];
+    localStorage.setItem('col-md-10 description', JSON.stringify(list));
 //textbox clears when saved
-})
+    $('#col-md-10 description').val('');
+    list.spice(blockHour, 1)
+}
