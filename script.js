e67760a9224b2acc2738da445c6e68e8562839b4
Champion113 <sbalbright19@gmail.com>
2020-09-08 14:08:57 -0400

    updated js

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
 $(document).ready(function(){
 
-var now = 
 
-$(".btn").on("click", function(){
+$(".saveBtn").on("click", function(){
 // things that happen on the button
 var activity = $(this).siblings(".description").val();
-var hour = 
 
 console.log(activity)
 })
-/////
-$(".textData").dblclick(function () {
-    $(this).addClass('d-none')
-    $(this).siblings("input").removeClass("d-none");
-});
 
-$(".btn").dblclick(function () {
-    
-    var id = $(this).attr("id")
-    var time = id.slice(0,3)
-    console.log(time)
-
-    var timeInputId = time + "Input"; 
-});
-
-
-//////
 //  funtion to be able to detect time using moments;
 $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
 
 // funtion to determine if time past present or future within hour timeframe
 var currentTime = moment().format('H');
-// set colors based on timeframe p-grey pr-blue f-purple
+// set colors based on timeframe p-grey pr-green f-red
+$('.colorcode').each(function name(params) {
+    
+})
 
 
-$
+
+//save to local storage
+//localStorage.setItem('col-md-10 description', JSON.stringify(list));
+//textbox clears when saved
 })
